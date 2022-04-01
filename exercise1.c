06d7dcbaa795852437893470e684192d1feff532
aqillakhamis <aqillakhamis@gmail.com>
2022-04-01 20:26:46 +0800

    update tutorial week 2

---
+++ exercise1.c
@@ -0,0 +1,19 @@
+//documentation section 
+/* Exercise 1 - 1D array */
+
+//pre-processor section 
+#include<stdio.h>
+
+//global variable section 
+
+
+//main function section 
+int main(){
+    //write here your program
+
+
+    return 0;
+}
+
+
+//user-defined section 
---
+++ exercise4.c
@@ -0,0 +1,19 @@
+//documentation section 
+/* Exercise 4 - 2D array */
+
+//pre-processor section 
+#include<stdio.h>
+
+//global variable section 
+
+
+//main function section 
+int main(){
+    //write here your program
+
+
+    return 0;
+}
+
+
+//user-defined section 
